86f8830146c651e31360d6e6436b24a4b06838ba
Psithinker <rinnengan50@gmail.com>
2022-07-04 01:28:50 -0700

    C Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <string.h>
 /**
- *  * main - main block
- *   * Return: 0
- *    */
+  * main - main block
+  * Return: 0
+  */
 
 int main(void)
 {
-		puts("\"Programming is like building a multilingual puzzle");
-			return (0);
+       puts("\"Programming is like building a multilingual puzzle");
+       return (0);
 }
