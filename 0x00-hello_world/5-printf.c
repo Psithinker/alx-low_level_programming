ee8ad31e70ddd38686f3b2c7f212978f3358e6a4
Psithinker <rinnengan50@gmail.com>
2022-07-04 03:05:22 -0700

    Task 5.

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
-#include <studio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0)
+	return (0);
 }
