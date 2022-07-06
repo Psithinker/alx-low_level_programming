3ba5b1144290d5c6ef6c2239f77554def150a67d
Psithinker <rinnengan50@gmail.com>
2022-07-05 20:14:44 -0700

    ok fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,7 @@
 #include "main.h"
 
 /**
-* print_alphabet - > prints the lowercase alphabets
-*
-* Return: always 0
+* print_alphabet - prints the lowercase alphabets
 */
 
 void print_alphabet(void)
@@ -15,7 +13,5 @@ void print_alphabet(void)
 	     _putchar(j);
 	}
 	_putchar('\n');
-
-	return (0);
 
 }
