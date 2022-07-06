f4b39c181e63c7dd74e24f11cf493d633910b16a
Psithinker <rinnengan50@gmail.com>
2022-07-06 10:40:58 -0700

    print_alphabet_×10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet_×10 - print 10 times the alphabet in lowercase
+ */
+void print_alphabet_×10(void)
+{
+	char c = 0;
+	
+	while (c <= 10)
+	{
+		_putchar(c);
+		c++
+	}
+	_putchar('\n');
+}
