99abcd632289ab91cadc1e311823688b88763661
Psithinker <rinnengan50@gmail.com>
2022-07-07 21:59:43 -0700

    Task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal (_) n number of times
+ * @n: integer variable
+ *
+ */
+
+void print_diagonal(int n)
+{
+	if (n < 1)
+		_putchar('\n');
+	else
+	{
+		int jj;
+
+		for (jj = 1; jj <= n; jj++)
+		{
+			int ii;
+
+			for (ii = 1; ii <= jj; ii++)
+			{
+				if (ii == jj
+					_putchar('\\');
+				else
+					_putchar(' ');
+			}
+		}
+	}
+}
