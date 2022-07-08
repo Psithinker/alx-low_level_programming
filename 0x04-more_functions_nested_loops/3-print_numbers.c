2afbef471dc9fc1808f4cd27a421bffa3c9666b1
Psithinker <rinnengan50@gmail.com>
2022-07-07 20:20:29 -0700

    hotfix 2

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * print_numbers - prints all numbers from 0 to 9
+ * print_numbers - prints number from 0-9
  *
  */
 
 void print_numbers(void)
 {
-	int ii = 0
+	int ii = 0;
 
 	do {
 		_putchar(ii + '0');
