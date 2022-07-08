dba5a2b2b338a69f43f95c74e30646743a7d75eb
Psithinker <rinnengan50@gmail.com>
2022-07-07 18:27:55 -0700

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints all numbers from 0-9
+ *
+ */
+
+void print_numbers(void)
+{
+	int ii = 0
+
+	do {
+		_putchar(ii + '0');
+		ii++
+	} while (ii < 10);
+
+	_putchar('\n');
+}
