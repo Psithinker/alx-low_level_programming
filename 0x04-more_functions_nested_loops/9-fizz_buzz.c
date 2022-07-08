67f3f4bc4fa890fff026569dab405c4e19832d14
Psithinker <rinnengan50@gmail.com>
2022-07-08 16:20:16 -0700

    Task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#define stopVar 100
+
+/**
+ * main - main function
+ * checks through numbers from 1 - 100
+ * and print number if it is not a multiple of 3 or 5
+ * print fizz if its a multiple of 3
+ * buzz if its a multiple of 5
+ * and fizz buzz if its both a multiple of 3 and 5
+ *
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	int ii;
+
+	do {
+		if ((ii % 3 == 0) && (ii % 5 == 0))
+			printf("FizzBuzz");
+		else if (ii % 3 == 0)
+			printf("Fizz");
+		else if (ii % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", ii);
+		if (ii != stopVar)
+			printf(" ");
+		ii++
+	} while (ii < stopVar + 1);
+	printf("\n");
+
+	return (0);
+}
