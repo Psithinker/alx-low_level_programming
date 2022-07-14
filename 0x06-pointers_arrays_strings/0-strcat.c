c0f1f1a6f852039a1b543ffe5cc532ef4f29eab7
Psithinker <rinnengan50@gmail.com>
2022-07-14 04:46:21 -0700

    strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+/**
+ * *_strcat - concatenates two strings
+ * @dest: pointer destination
+ * @src: poiner source
+ * Return: void
+ */
+
+char *_strcat(char *dest, char *src)
+{
+int a = -1, i;
+for (i = 0; dest[i] != '\0'; i++)
+;
+
+do {
+	a++;
+	dest[i] = src[a];
+	i++
+} while (src[a] != '\0');
+
+return (dest);
+}
