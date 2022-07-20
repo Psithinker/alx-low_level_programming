1ce7a42b717f177dd83d4d6a0fb760a8bf1b7fcb
Psithinker <rinnengan50@gmail.com>
2022-07-20 10:20:47 -0700

    Task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,10 +6,8 @@
  */
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
-	    return;
-	s++;
-	_print_rev_recursion(s);
-	s--;
+if (*s > '\0')
+{
+	_print_rev_recursion(s + 1);
 	_putchar(*s);
 }
