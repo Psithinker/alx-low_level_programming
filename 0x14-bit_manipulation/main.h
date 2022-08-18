03b0ab1034040ccd2983374ffff254f4fcb97a01
Psithinker <rinnengan50@gmail.com>
2022-08-18 15:49:49 -0700

    Task 0

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,11 @@
+#ifndef LIST_H
+#define LIST_H
+#include<stdio.h>
+#include<string.h>
+#include<limits.h>
+#define MAX_COUNT (sizeof(unsigned long int) * 8)
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+
+#endif
